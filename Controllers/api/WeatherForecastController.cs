2cb02fd56c03c62095c99255fd1f304f95a40127
nofear195 <aaasss777as@gmail.com>
2023-05-18 04:37:13 +0800

    fix some error

--- Controllers/api/WeatherForecastController.cs
+++ Controllers/api/WeatherForecastController.cs
@@ -16,37 +16,37 @@ namespace dotnet_project_template.Controllers.api
         [HttpGet]
         public IEnumerable<string> Get()
         {
-            //string connectionString = "Server=localhost;Port=5432;Database=postgres;User Id=postgres;Password=;";
-            //NpgsqlConnection? connection = null;
-            //try
-            //{
-            //    connection = new NpgsqlConnection(connectionString);
-            //}
-            //catch (Exception e)
-            //{
-            //    Console.WriteLine("error" + e);
-            //}
+            string connectionString = "Server=localhost;Port=5432;Database=postgres;User Id=postgres;Password=;";
+            NpgsqlConnection? connection = null;
+            try
+            {
+                connection = new NpgsqlConnection(connectionString);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("error" + e);
+            }
 
-            //connection.Open();
+            connection.Open();
 
-            //using (NpgsqlCommand command = new NpgsqlCommand())
-            //{
-            //    command.Connection = connection;
+            using (NpgsqlCommand command = new NpgsqlCommand())
+            {
+                command.Connection = connection;
 
-            //    // Create the Student table
-            //    command.CommandText = @"
-            //        CREATE TABLE IF NOT EXISTS Student (
-            //            Id SERIAL PRIMARY KEY,
-            //            Name VARCHAR(255),
-            //            Age INT
-            //        );";
+                // Create the Student table
+                command.CommandText = @"
+                    CREATE TABLE IF NOT EXISTS Student (
+                        Id SERIAL PRIMARY KEY,
+                        Name VARCHAR(255),
+                        Age INT
+                    );";
 
-            //    command.ExecuteNonQuery();
-            //}
+                command.ExecuteNonQuery();
+            }
 
-            //Console.WriteLine("Student table created successfully.");
+            Console.WriteLine("Student table created successfully.");
 
-            //connection.Close();
+            connection.Close();
             return new string[] { "value1", "value2" };
         }
 
--- DataContext/AppContext.cs
+++ DataContext/AppContext.cs
@@ -1,3 +1,5 @@
+using Microsoft.EntityFrameworkCore;
+
 namespace dotnet_project_template.DataContext
 {
     public class AppContext:DbContext
