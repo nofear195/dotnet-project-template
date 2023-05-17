e289dfd9ad468f5b399928baaa09cc4859a6a2c3
nofear195 <aaasss777as@gmail.com>
2023-05-18 03:46:12 +0800

    init commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace dotnet_project_template.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
