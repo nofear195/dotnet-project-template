c2409b823ec5234fe70126f8cdc20545d24c3abf
nofear195 <aaasss777as@gmail.com>
2023-05-18 20:46:36 +0800

    use configuration to get variable from appsettings.json

--- Controllers/api/WeatherForecastController.cs
+++ Controllers/api/WeatherForecastController.cs
@@ -4,50 +4,57 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Npgsql;
+using Microsoft.Extensions.Configuration;
 
-// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace dotnet_project_template.Controllers.api
 {
     [Route("api/[controller]")]
     public class WeatherForecastController : Controller
     {
+        private readonly IConfiguration Configuration;
+
+        public WeatherForecastController(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
         // GET: api/values
         [HttpGet]
         public IEnumerable<string> Get()
         {
-            string connectionString = "Server=localhost;Port=5432;Database=postgres;User Id=postgres;Password=;";
-            NpgsqlConnection? connection = null;
-            try
-            {
-                connection = new NpgsqlConnection(connectionString);
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine("error" + e);
-            }
+            var configVal = Configuration["ConnectionStrings:DefaultConnection"];
+            //string connectionString = "Server=localhost;Port=5432;Database=postgres;User Id=postgres;Password=;";
+            //NpgsqlConnection? connection = null;
+            //try
+            //{
+            //    connection = new NpgsqlConnection(connectionString);
+            //}
+            //catch (Exception e)
+            //{
+            //    Console.WriteLine("error" + e);
+            //}
 
-            connection.Open();
+            //connection.Open();
 
-            using (NpgsqlCommand command = new NpgsqlCommand())
-            {
-                command.Connection = connection;
+            //using (NpgsqlCommand command = new NpgsqlCommand())
+            //{
+            //    command.Connection = connection;
 
-                // Create the Student table
-                command.CommandText = @"
-                    CREATE TABLE IF NOT EXISTS Student (
-                        Id SERIAL PRIMARY KEY,
-                        Name VARCHAR(255),
-                        Age INT
-                    );";
+            //    // Create the Student table
+            //    command.CommandText = @"
+            //        CREATE TABLE IF NOT EXISTS Student (
+            //            Id SERIAL PRIMARY KEY,
+            //            Name VARCHAR(255),
+            //            Age INT
+            //        );";
 
-                command.ExecuteNonQuery();
-            }
+            //    command.ExecuteNonQuery();
+            //}
 
-            Console.WriteLine("Student table created successfully.");
+            //Console.WriteLine("Student table created successfully.");
 
-            connection.Close();
-            return new string[] { "value1", "value2" };
+            //connection.Close();
+            return new string[] { "value1", "value2",configVal };
         }
 
         // GET api/values/5
